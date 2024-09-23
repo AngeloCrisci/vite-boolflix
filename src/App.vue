41e164fe7e018af4ab3c374692598d209270c71a
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-23 12:58:48 +0200

    Connected all header components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script>
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
 
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  }
+}
 </script>
 
 <template>
-
+  <AppMain />
+  <AppHeader />
 </template>
 
-<style></style>
+<style lang="scss">
+@use 'style.css';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+import HeaderButton from './HeaderButton.vue'
+import HeaderSearchBar from './HeaderSearchBar.vue'
+export default {
+    name: 'AppHeader',
+    components: {
+        HeaderButton,
+        HeaderSearchBar,
+    }
+}
+</script>
+
+<template>
+    <h1>BOOLFLIX</h1>
+    <HeaderButton />
+    <HeaderSearchBar />
+</template>
+
+<style></style>
