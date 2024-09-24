5c5da0a9c9beb8596da54c35c627ca3d512c4d16
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-09-24 12:43:03 +0200

    Milestone 3 completata

--- src/components/ContentMain.vue
+++ src/components/ContentMain.vue
@@ -1,4 +1,5 @@
 <script>
+import notFound from '../assets/notfound.jpg'
 import { store } from '../store';
 
 export default {
@@ -25,6 +26,16 @@ export default {
             };
             const countryInitial = changeFlag[languageCode] || languageCode;
             return `https://flagcdn.com/w40/${countryInitial}.png`;
+        },
+        getStars(vote) {
+            return Math.round(vote / 2);
+        }
+    },
+    computed: {
+        getPoster() {
+            const url = "https://image.tmdb.org/t/p/w342/"
+            const urlImage = this.result.poster_path
+            return this.result.poster_path ? `${url}${urlImage}` : notFound;
         }
     }
 }
@@ -38,8 +49,15 @@ export default {
         <li>
             <img :src="getFlag(result.original_language)" alt="flag">
         </li>
-        <li>{{ result.title }}</li>
-        <li> {{ result.vote_average }}</li>
+        <li>
+            <img :src="getPoster" alt="Poster">
+        </li>
+        <li>
+            <p class="stars mb-2">
+                <i v-for="n in 5" :class="n <= getStars(result.vote_average) ? 'bi bi-star-fill' : 'bi bi-star'"
+                    :key="n"></i>
+            </p>
+        </li>
     </ul>
 </template>
 
